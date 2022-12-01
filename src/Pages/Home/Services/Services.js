3d60aa2433a2554a4952f1fa194f2112b1772584
mohon63 <mohonhossen63@gmail.com>
2022-12-01 23:09:25 +0600

    genious car service

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import repair1 from '../../../images/repair1.png';
+import Service from '../Service/Service';
 
 const Services = () => {
 
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
     return (
         <div>
-            <h2>Services: </h2>
-            <h2>Services: </h2>
-            <h2>Services: </h2>
+            <h2>Services: {services.length}</h2>
+            {
+                services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                ></Service>)
+            }
         </div>
     );
 };
