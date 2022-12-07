807523428cf971349ddb10eddb43fab29f44d574
mohon63 <mohonhossen63@gmail.com>
2022-12-07 23:51:29 +0600

    hedder

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
 
     return (
         <div>
-            <h1 className='services-title'>Services</h1>
+            <h1 className='services-title mt-5'>Services</h1>
             <div className="services-container">
                 {
                     services.map(service => <Service
