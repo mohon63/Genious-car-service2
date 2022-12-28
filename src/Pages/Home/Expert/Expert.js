832826f27a759b6b14e608ae77f7da8a2632bc64
mohon63 <mohonhossen63@gmail.com>
2022-12-28 22:25:23 +0600

    hh

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -8,7 +8,7 @@ const Expert = ({ expert }) => {
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
-                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
