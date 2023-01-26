9eb45cd96f5f4b916586c9ca685865ebf67dca0a
mohon63 <mohonhossen63@gmail.com>
2023-01-26 23:23:15 +0600

    JJ

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -11,6 +11,7 @@ const Expert = ({ expert }) => {
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
